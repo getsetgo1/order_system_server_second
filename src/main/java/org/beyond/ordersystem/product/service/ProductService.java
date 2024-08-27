9e0868a0f5e9705eb473dfc3b87655fe7a56b308
getsetgo1 <dnwls1313@gmail.com>
2024-08-27 18:58:48 +0900

    fix

--- src/main/java/org/beyond/ordersystem/product/service/ProductService.java
+++ src/main/java/org/beyond/ordersystem/product/service/ProductService.java
@@ -57,7 +57,7 @@ public class ProductService {
 
 
             byte[] bytes = image.getBytes();
-            Path path = Paths.get("/tmp", product.getId() + "_" + image.getOriginalFilename());
+            Path path = Paths.get("/tmp/", product.getId() + "_" + image.getOriginalFilename());
             Files.write(path, bytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
 
             product.updateImagePath(path.toString()); // dirty checking
@@ -87,7 +87,8 @@ public class ProductService {
             String fileName = product.getId() + "_" + image.getOriginalFilename();
             // 로컬 PC에 임시저장. 파일로 바꾸고 올리기 위해서!!
             // 프론트엔드에서 직접 올릴 때에는 이렇게 변환해줄 필요가 없다. 파일로 가지고 있으니까
-            Path path = Paths.get("/Users/sejeong/Documents/temp", fileName);
+            Path path = Paths.get("/tmp/", product.getId() + "_" + image.getOriginalFilename());
+            // Path path = Paths.get("/Users/sejeong/Documents/temp", fileName);
             // local pc에 임시 저장
             Files.write(path, bytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
 
